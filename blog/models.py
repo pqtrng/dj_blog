795416ad224e3d1907f31988776b70faa9e8ca2b
Pham, Trung <pqtrng@email.com>
2021-06-30 15:29:53 +0700

    Create Post model

--- blog/models.py
+++ blog/models.py
@@ -9,4 +9,21 @@ class Post(models.Model):
         ('published', 'Published'),
     )
 
-    title = models.CharField(max_length=250)
+    title = models.CharField(max_length=250)  # post title
+    slug = models.SlugField(
+        max_length=250, unique_for_date='publish')  # for URL
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts')
+    body = models.TextField()  # body of the post
+    publish = models.DateTimeField(default=timezone.now)  # time of publishing
+    created = models.DateTimeField(auto_now_add=True)  # time of creation
+    updated = models.DateTimeField(auto_now=True)  # time of update
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    class Meta:
+        # sort results by publish field in descending order (-) when query database
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title  # for better representation in adminitration site
