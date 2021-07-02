c25267294dd9ccbb67017040397b965d17faaf65
Pham, Trung <pqtrng@email.com>
2021-07-02 20:30:26 +0700

    Add Show Latest Post templatetags
    Tag allows to show n latest posts.

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -3,6 +3,7 @@ from ..models import Post
 
 register = template.Library()
 
+
 @register.simple_tag
 def total_posts():
     """A customized tag total_posts and register the function as a simple tag
@@ -11,3 +12,9 @@ def total_posts():
         [type]: [description]
     """
     return Post.published.count()
+
+
+@register.inclusion_tag(filename='blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
