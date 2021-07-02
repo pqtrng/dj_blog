59b489a83411d08c6e5f22bacec7526b3682d26d
Pham, Trung <pqtrng@email.com>
2021-07-02 20:48:51 +0700

    Add MostCommentedTag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from ..models import Post
+from django.db.models import Count
 
 register = template.Library()
 
@@ -9,7 +10,7 @@ def total_posts():
     """A customized tag total_posts and register the function as a simple tag
 
     Returns:
-        [type]: [description]
+        int: The number of published posts
     """
     return Post.published.count()
 
@@ -18,3 +19,8 @@ def total_posts():
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
